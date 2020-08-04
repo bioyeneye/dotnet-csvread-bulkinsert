a3c973f30828d23681032f0b0fe79c08b5b75b1d
Bolaji Oyeneye <bioyeneye@gmail.com>
2020-08-04 22:47:45 +0100

    tested bulk insert

--- ReadCSVDbBulkInsert/ReadCSVDbBulkInsert/Program.cs
+++ ReadCSVDbBulkInsert/ReadCSVDbBulkInsert/Program.cs
@@ -10,7 +10,28 @@ namespace ReadCSVDbBulkInsert
         {
             var data = CsvData.LoadCsvData("data.csv");
             
-            
+            string connectionString = @"Data Source = MyServerName/Instance; Integrated Security=true; Initial Catalog=YourDatabase";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (SqlBulkCopy bulkCopy = new SqlBulkCopy(connection))
+                {
+                    foreach (DataColumn c in data.Columns)
+                        bulkCopy.ColumnMappings.Add(c.ColumnName, c.ColumnName);
+ 
+                    bulkCopy.DestinationTableName = data.TableName;
+                    try
+                    {
+                        bulkCopy.WriteToServer(data.CreateDataReader());
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+
+            var dataRows =  data.Rows;
             Console.WriteLine();
             Console.WriteLine("Hello World!");
         }
